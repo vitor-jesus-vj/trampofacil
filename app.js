2f2054e2360a0a6bd0d06bc0a3c6b7853db77ff1
vitor-jesus-vj <vitorvj100@gmail.com>
2025-04-17 16:59:55 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// Substitua pelas suas credenciais reais do Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  projectId: "SEU_PROJETO_ID",
+  storageBucket: "SEU_BUCKET",
+  messagingSenderId: "SEU_MENSAGER_ID",
+  appId: "SEU_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
